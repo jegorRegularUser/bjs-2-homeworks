4b6eb126e9189e8cda6aa7bd1cceeadf68fb93f6
Jegor <140427023+jegorRegularUser@users.noreply.github.com>
2024-03-29 01:25:55 +0300

    Домашнее задание к лекции 2 Функции сделано

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,27 +1,32 @@
 function getArrayParams(...arr) {
-  const max = arr[0];
-  const min = arr[1];
-  const avg = +(arr.reduce((acc, el)=> acc+el)/arr.length).toFixed(2);
+  const max = Math.max(...arr);
+  const min = Math.min(...arr);
+  const avg = +(arr.reduce((acc, el) => acc + el) / arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
-
+  return arr.length != 0 ? arr.reduce((acc, el) => acc + el, 0) : 0;
 }
 
 function differenceMaxMinWorker(...arr) {
+  return arr.length != 0 ? Math.max(...arr) - Math.min(...arr) : 0;
 
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  return arr.length != 0 ? arr.reduce((acc, el) => el % 2 == 0 ? acc + el : acc + 0, 0) - arr.reduce((acc, el) => el % 2 != 0 ? acc + el : acc + 0, 0) : 0;
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  return arr.length != 0 ? arr.reduce((acc, el) => el % 2 == 0 ? acc + el : acc + 0, 0) / arr.reduce((acc, el) => el % 2 == 0 ? acc + 1 : acc + 0, 0) : 0;
 }
 
-function makeWork (arrOfArr, func) {
-
+function makeWork(arrOfArr, func) {
+  const arrWithResults =[];
+  for (i=0;i<arrOfArr.length; i++){ 
+    arrWithResults.push(func(...arrOfArr[i]))
+  };
+  return Math.max(...arrWithResults);
 }
