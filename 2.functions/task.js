dd3a5896008055ce032aa1429057e569604baa0d
Jegor <140427023+jegorRegularUser@users.noreply.github.com>
2024-03-29 00:17:52 +0300

    git test

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,7 @@
 function getArrayParams(...arr) {
+  const max = arr[0];
+  const min = arr[1];
+  const avg = +(arr.reduce((acc, el)=> acc+el)/arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
