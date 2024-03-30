94a898de422ff05f354e367aca09ea4cb280194a
Jegor <140427023+jegorRegularUser@users.noreply.github.com>
2024-03-30 18:31:38 +0300

    Домашнее задание к лекции 3. «Массивы» выполнено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -3,11 +3,12 @@ function compareArrays(arr1, arr2) {
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-    console.log(users)
     if (users.length == 0) {
         return 0
     } else {
         filteredByGenderUsers = users.filter(el => el.gender == gender)
-        return filteredByGenderUsers.reduce((acc, el) =>{console.log(el.age); acc + el.age, 0}) / filteredByGenderUsers.length
+        return filteredByGenderUsers.length === 0 ? 0 : filteredByGenderUsers.reduce((acc, el) => acc + el.age, 0) / filteredByGenderUsers.length;
+
     }
+
 }
